a94716ffc728100d80ce50c4157a5dce20a19b8f
ikhidebright <ikhidebright@gmail.com>
2020-06-12 04:27:18 +0100

    added mongo atlas connection

--- server/server.js
+++ server/server.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const { connect } = require('mongoose')
+const dotenv = require("dotenv")
+
+dotenv.config()
 
 const app = express()
+
+connect(process.env.database,
+{ useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log("connected db")
+    }
+})
 
 // middleware
 app.use(morgan('dev'))

851045da165277f1a3754870de1937cd77233454
ikhidebright <ikhidebright@gmail.com>
2020-06-12 03:18:30 +0100

    res

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+// middleware
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+    res.json('Hello Amazon clone')
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+});
+
+app.listen(3000, (err) => {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log('Port Running')
+    }
+})
